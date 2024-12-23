5aa849477b123b625aa64da09627a2ee69cd1d3a
Marwa Sayed <marwa_sayed88@outlook.com>
2024-12-23 23:31:31 +0430

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function getElement(selection) {
       `Please check "${selection}" selector, no such element exists`
     );
   }
-  
+   
   function Gallery(element) {
     this.container = element;
     this.list = [...element.querySelectorAll('.img')];
